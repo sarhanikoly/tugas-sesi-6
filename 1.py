6c6165dc5f7259667065a07db36488e8c9c5c25c
sarhanikoly <160218232+sarhanikoly@users.noreply.github.com>
2024-04-01 01:50:11 +0700

    latihan advance loop
    nama : desvita sarani koly
    nim  : 20230040278
    kelas : ti23a

---
+++ 1.py
@@ -0,0 +1,16 @@
+print("*")
+
+print("*")
+
+for i in range(2):
+    print("*",end="")
+print()
+
+
+for i in range(5):
+    print("*",end="")
+print()
+
+for i in range(8):
+    print("*",end="")
+print()
---
+++ 2.py
@@ -0,0 +1,20 @@
+for i in range(1, 6):
+    print(i, end=" ")
+print()
+
+for i in range(2, 11, 2):
+    print(i, end=" ")
+print()
+
+for i in range (3,16,3):
+    print(i,end=" ")
+print()
+
+for i in  range(4,21,4):
+    print(i,end=" ")
+print()
+
+for i in range (5,26,5):
+    print(i,end=" ")
+print()
+
